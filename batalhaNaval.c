a75542a7d6f34cfd6d822606cbd44bad8441324b
Pedro Canuto <data.canuto@gmail.com>
2026-07-20 10:20:51 -0300

    Update batalhaNaval.c
    Desafio Avancado

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,169 +1,153 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define S 5
+#define T 10
+#define N 3
 
 int main(){
-    printf("\n===Batalha Naval===\n");
-
-    printf("\nTabuleiro Vazio: \n");
-
-    int tabuleiro[10][10];
+    //Inicialização do tabuleiro
+    int tabuleiro[T][T];
     
-    int navio1[3] = {3,3,3};
-    int navio2[3] = {3,3,3};
-    int navio3[3] = {3,3,3};
-    int navio4[3] = {3,3,3};
-    
-    int linComecoNav1, colComecoNav1, linComecoNav2, colComecoNav2,linComecoNav3, colComecoNav3, linComecoNav4, colComecoNav4;
-    char orientacaoNav1, orientacaoNav2, orientacaoNav3, orientacaoNav4;
-
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+    for(int i = 0; i < T; i++){
+        for (int j = 0; j < T; j++){
             tabuleiro[i][j] = 0;
         }
     }
-    printf("\n");
-    printf("\n---Batlha Naval---\n");
-
-    printf("\nTabuleiro Vazio: \n");
-
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j< 10; j++){
+    
+    //Visualizando tabuleiro vazio
+    printf("\n===Batalha Naval===\n");
+    printf("\nTabuleiro Vazio\n");
+    
+    for(int i = 0; i < T; i++){
+        for(int j = 0; j < T; j++){
             printf("%d ",tabuleiro[i][j]);
         }
-        printf("\n ");
+        printf("\n");
     }
-
-    printf("\nVamos posicionar 4 navios no tabuleiro!\n");
-
-    
-    
-    printf("\nInforme em qual linha deseja posicionar o navio 1[1 a 10]: ");
-    scanf("%d",&linComecoNav1);
-    printf("\nInforme em qual coluna deseja posicionar o navio 1[1 a 10]: ");
-    scanf("%d",&colComecoNav1);
-    printf("Informe se o navio 1 estará na diagnoal, horizontal ou na vertical[H, V]: ");
-    scanf(" %c",&orientacaoNav1);
-
     printf("\n");
-
-    printf("\nInforme em qual linha deseja posicionar o navio 2: ");
-    scanf("%d",&linComecoNav2);
-    printf("\nInforme em qual coluna deseja posicionar o navio 2: ");
-    scanf("%d",&colComecoNav2);
-    printf("Informe se o navio 2 estará na diagnoal, horizontal ou na vertical: ");
-    scanf(" %c",&orientacaoNav2);
-
+    
+    //Definindo Skills:
+    int cone[S][S];
+    
+    for (int i = 0; i < S; i++){
+        for(int j = 0; j < S; j++){
+            if(j >= (S/2 - i) && j <= (S/2 + i) && i <= S/2){
+                cone[i][j] = 1;
+            }else{
+                cone[i][j] = 0;
+            }
+        }
+    }
+    int cruz[S][S];
+    
+    for (int i = 0; i < S; i++){
+        for (int j = 0; j < S; j++){
+            if(i == S/2 || j == S/2){
+                cruz[i][j] = 1;
+            }else{
+                cruz[i][j] = 0;
+            }
+        }
+    }
+    int octaedro[S][S];
+    
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < S; j++) {
+            if (abs(i - S/2) + abs(j - S/2) <= S/2) {
+                octaedro[i][j] = 1;
+            } else {
+                octaedro[i][j] = 0;
+            }
+        }
+    }
+    
+    //Inicializando navios
+    int navioHorizontal[N] = {3,3,3};
+    int navioVertical[N] = {3,3,3};
+    int navioDiagonal[N] = {3,3,3};
+    
+    //Posicionando navios no tabuleiro:
+    
+    //Navio 1: Linha: 1, Coluna: 2, Orientação: Horizontal
+    for(int i = 0; i < N; i++){
+        tabuleiro[1][2 + i] = navioHorizontal[i];
+    }
+    //Navio 2: Linha 2, coluna 2, Orientação: Vertical
+    for(int i = 0; i < N; i++){
+        tabuleiro[2 + i][7] = navioVertical[i];
+    }
+    //Navio 3: Linha 7, coluna 2, Orientação: Diagonal
+    for(int i = 0; i < N; i++){
+        tabuleiro[4 + i][6 + i] = navioDiagonal[i];
+    }
+    
     printf("\n");
-
-    printf("\nInforme em qual linha deseja posicionar o navio 3[1 a 10]: ");
-    scanf("%d",&linComecoNav3);
-    printf("\nInforme em qual coluna deseja posicionar o navio 3: ");
-    scanf("%d",&colComecoNav3);
-    printf("Informe se o navio 3 estará na diagnoal, horizontal ou na vertical: ");
-    scanf(" %c",&orientacaoNav3);
-
-    printf("\n");
-
-    printf("\nInforme em qual linha deseja posicionar o navio 4[1 a 10]: ");
-    scanf("%d",&linComecoNav4);
-    printf("\nInforme em qual coluna deseja posicionar o navio 4: ");
-    scanf("%d",&colComecoNav4);
-    printf("Informe se o navio 4 estará na diagnoal, horizontal ou na vertical: ");
-    scanf(" %c",&orientacaoNav4);
-
     
-    printf("\nPosicionando os navios no tabuleiro...\n");
-    switch(orientacaoNav1){
-        case 'H':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav1][colComecoNav1 + i] = navio1[i];
-            }
-            break;
-        }
-        case 'V':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav1 + i][colComecoNav1] = navio1[i];
-            }
-            break;
-        }
-        case 'D':{
-            for(int i = 0; i < 3; i++){
-                tabuleiro[linComecoNav1 + i][colComecoNav1 + i] = navio1[i];
-            }
-        }
-    }
-    switch(orientacaoNav2){
-        case 'H':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav2][colComecoNav2 + i] = navio2[i];
-            }
-            break;
-        }
-        case 'V':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav2 + i][colComecoNav2] = navio2[i];
-            }
-            break;
-        }
-        case 'D':{
-            for(int i = 0; i < 3; i++){
-                tabuleiro[linComecoNav2 + i][colComecoNav2 + i] = navio2[i];
-            }
-        }
-    }
-
-    switch(orientacaoNav3){
-        case 'H':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav3][colComecoNav3 + i] = navio3[i];
-            }
-            break;
-        }
-        case 'V':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav3 + i][colComecoNav3] = navio3[i];
-            }
-            break;
-        }
-        case 'D':{
-            for(int i = 0; i < 3; i++){
-                tabuleiro[linComecoNav3 + i][colComecoNav3 + i] = navio3[i];
-            }
-        }
-    }
-    switch(orientacaoNav4){
-        case 'H':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav4][colComecoNav4 + i] = navio4[i];
-            }
-            break;
-        }
-        case 'V':{
-            for(int i = 0; i < 3; i++){
-            tabuleiro[linComecoNav4 + i][colComecoNav4] = navio4[i];
-            }
-            break;
-        }
-        case 'D':{
-            for(int i = 0; i < 3; i++){
-                tabuleiro[linComecoNav4 + i][colComecoNav4 + i] = navio4[i];
-            }
-        }
-    }
-    
-    printf("\n\n");
-    for(int i = 0; i < 10; i++){
-        printf(" %c ", 'A'+i);
-    }
-    printf("\n\n\n");
-    
-    for(int i = 0; i < 10; i++){
-        printf("%4d - ",i+1);
-        
-        for (int j = 0; j < 10; j++){
+    printf("\n===Batalha Naval===\n");
+    printf("\nTabuleiro Com Navios\n");
+    //Visualizando tabuleiro após posicionar os barcos
+    for(int i = 0; i < T; i++){
+        for(int j = 0; j < T; j++){
             printf("%d ",tabuleiro[i][j]);
         }
-        printf("\n ");
+        printf("\n");
     }
-            
-    return 0;
+    
+    // Origem das habilidades no tabuleiro
+    int origemConeLin = 2, origemConeCol = 2;
+    int origemCruzLin = 6, origemCruzCol = 6;
+    int origemOctLin = 5, origemOctCol = 2;
+    //Posicionando skills no tabuleiro
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < S; j++) {
+            int lin = origemConeLin + i - S/2;
+            int col = origemConeCol + j - S/2;
+            if (lin >= 0 && lin < T && col >= 0 && col < T && cone[i][j] == 1) {
+                tabuleiro[lin][col] = 5;
+            }
+        }
+    }
+
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < S; j++) {
+            int lin = origemCruzLin + i - S/2;
+            int col = origemCruzCol + j - S/2;
+            if (lin >= 0 && lin < T && col >= 0 && col < T && cruz[i][j] == 1) {
+                tabuleiro[lin][col] = 5;
+            }
+        }
+    }
+
+    for (int i = 0; i < S; i++) {
+        for (int j = 0; j < S; j++) {
+            int lin = origemOctLin + i - S/2;
+            int col = origemOctCol + j - S/2;
+            if (lin >= 0 && lin < T && col >= 0 && col < T && octaedro[i][j] == 1) {
+                tabuleiro[lin][col] = 5;
+            }
+        }
+    }
+    
+    printf("\n");
+    
+    printf("\n===Batalha Naval===\n");
+    printf("\nTabuleiro Com Navios e Habilidades\n");
+    //Visualizando tabuleiro após posicionar as skills!
+    for(int i = 0; i < T; i++){
+        for(int j = 0; j < T; j++){
+            printf("%d ",tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    
+    
+    
+   
+    
+
+    
+    
 }
